f2f2bdd00945a7b3e1bad4ef5b4cc0cddffb265a
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-01 19:27:04 +0200

    Zavrsetak HttpGet rute

---
+++ BACKEND/Controllers/ProgramController.cs
@@ -0,0 +1,37 @@
+﻿using BACKEND.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ProgramController : ControllerBase
+    {
+        // 1. koristimo dependency injection
+
+        private readonly EdunovaContext _context;
+
+        // 2. u konstruktoru postavljamo vrijednost
+        public ProgramController(EdunovaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_context.Programi);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+
+
+
+    }
+}
