0025d9299893508653900d0bc0a62219f3aebed3
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-01 18:22:36 +0200

    Model klase

---
+++ BACKEND/Models/Entitet.cs
@@ -0,0 +1,7 @@
+﻿namespace BACKEND.Models
+{
+    public abstract class Entitet
+    {
+        public int Sifra { get; set; }
+    }
+}
---
+++ BACKEND/Models/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BACKEND.Models
+{
+    public class Program : Entitet
+    {
+
+        public string Naziv { get; set; } = "";
+        public decimal? Cijena { get; set; }
+        public bool Aktivan { get; set; }
+
+
+    }
+}
