1c3b831e71542e0c71b098686d0837535319830d
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-01 18:57:05 +0200

    Postavljanje rada s bazom

---
+++ BACKEND/Data/EdunovaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BACKEND.Data
+{
+    public class EdunovaContext : DbContext
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext> options) : base(options)
+        {
+            // ovdje se mogu fino postaviti opcije, ali ne za sada
+        }
+
+        public DbSet<BACKEND.Models.Program> Programi { get; set; }
+    }
+}
