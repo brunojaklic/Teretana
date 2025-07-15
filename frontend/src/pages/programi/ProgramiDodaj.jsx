3288bd900a355df3d41ddfe799886de6e136bb4a
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-15 18:48:18 +0200

    Završetak dodavanja novog programa

--- frontend/src/pages/programi/ProgramiDodaj.jsx
+++ frontend/src/pages/programi/ProgramiDodaj.jsx
@@ -1,13 +1,40 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { RouteNames } from "../../constants";
+import ProgramService from "../../services/ProgramService";
 
 
 export default function ProgramiDodaj() {
+
+
+    const navigate = useNavigate();
+
+    async function dodaj(program) {
+        const odgovor = await ProgramService.dodaj(program);
+        navigate(RouteNames.PROGRAM_PREGLED);
+    }
+
+
+    function odradiSubmit(e) { // e je event
+        e.preventDefault();
+
+        let podaci = new FormData(e.target); // dohvacamo sve podatke iz forme
+
+        dodaj(
+            {
+                naziv: podaci.get('naziv'),
+                cijena: parseFloat(podaci.get('cijena')),
+                aktivan: podaci.get('aktivan') == 'on'
+            }
+        )
+
+
+    }
+
     return (
         <>
             Dodavanje programa
-            <Form>
+            <Form onSubmit={odradiSubmit}>
                 <Form.Group controlId="naziv">
                     <Form.Label>Naziv</Form.Label>
                     <Form.Control type="text" name="naziv" required />
@@ -17,13 +44,13 @@ export default function ProgramiDodaj() {
                     <Form.Label>Cijena</Form.Label>
                     <Form.Control type="number" name="cijena" step={0.01} />
                 </Form.Group>
-                
+
                 <Form.Group controlId="aktivan">
                     <Form.Label>Aktivan</Form.Label>
-                    <Form.Check Label="aktivan" name="aktivan" />
+                    <Form.Check label="Aktivan" name="aktivan" />
                 </Form.Group>
 
-                <hr style={{marginTop: '50px'}}/>
+                <hr style={{ marginTop: '50px' }} />
                 <Row>
                     <Col xs={6} sm={6} md={3} lg={2} xl={6} xxl={6}>
                         <Link to={RouteNames.PROGRAM_PREGLED}
--- frontend/src/services/ProgramService.js
+++ frontend/src/services/ProgramService.js
@@ -1,17 +1,25 @@
 import { HttpService } from "./HttpService"
 
 
-async function get(){
+async function get() {
     return await HttpService.get('/Program')
     // sve je u redu, dobili smo odgovor
     .then((odgovor)=>{
-        // console.log(odgovor.data)
+        //console.log(odgovor.data)
         return odgovor.data
     })
     // nastala je greška, obradi ju
     .catch((e)=>{})
 }
 
+
+async function dodaj(program) {
+    return await HttpService.post('/Program',program)
+    .then((odgovor)=>{return true})
+    .catch((e)=>{return false})
+}
+
 export default{
-    get
+    get,
+    dodaj
 }
