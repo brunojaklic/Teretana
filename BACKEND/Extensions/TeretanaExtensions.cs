a9c720b66be0883a0e5d63fbd75ea044bb0fe11f
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-08-19 18:30:33 +0200

    Dodavanje TeretanaExtensions i ažuriranje konfiguracije servisa

---
+++ BACKEND/Extensions/TeretanaExtensions.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text;
+
+namespace BACKEND.Extensions
+{
+    public static class TeretanaExtensions
+    {
+
+        public static void AddTeretanaSwaggerGen(this IServiceCollection Services)
+        {
+
+            Services.AddSwaggerGen(sgo =>
+            {
+                var o = new OpenApiInfo()
+                {
+                    Title = "Teretana API",
+                    Version = "v1",
+                    Contact = new OpenApiContact()
+                    {
+                        Email = "bruno.jaklic.2005@gmail.com",
+                        Name = "Bruno Jaklić"
+                    },
+                    Description = "Ovo je dokumentacija za Teretana API",
+                    License = new OpenApiLicense()
+                    {
+                        Name = "Edukacijska licenca"
+                    }
+                };
+                sgo.SwaggerDoc("v1", o);
+
+                sgo.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Autorizacija radi tako da se prvo na ruti /api/v1/Autorizacija/token.  
+                              autorizirate i dobijete token (bez navodnika). Upišite 'Bearer' [razmak] i dobiveni token.
+                              Primjer: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2OTc3MTc2MjksImV4cCI6MTY5Nzc0NjQyOSwiaWF0IjoxNjk3NzE3NjI5fQ.PN7YPayllTrWESc6mdyp3XCQ1wp3FfDLZmka6_dAJsY'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                sgo.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                            {
+                              new OpenApiSecurityScheme
+                              {
+                                Reference = new OpenApiReference
+                                  {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                  },
+                                  Scheme = "oauth2",
+                                  Name = "Bearer",
+                                  In = ParameterLocation.Header,
+
+                                },
+                                new List<string>()
+                              }
+                    });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+
+            });
+
+        }
+
+        public static void AddTeretanaCORS(this IServiceCollection Services)
+        {
+
+            Services.AddCors(opcije =>
+            {
+                opcije.AddPolicy("CorsPolicy",
+                    builder =>
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+                );
+
+            });
+
+        }
+
+        public static void AddTeretanaSecurity(this IServiceCollection Services)
+        {
+            Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MojKljucKojijeJakoTajan i dovoljno dugačak da se može koristiti")),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = false
+                };
+            });
+
+        }
+
+    }
+}
--- BACKEND/Program.cs
+++ BACKEND/Program.cs
@@ -1,72 +1,50 @@
 ﻿using BACKEND.Data;
+using BACKEND.Extensions;
 using BACKEND.Mapping;
 using Microsoft.EntityFrameworkCore;
 
-// Dobro dodatno �itanje
-// 1. https://medium.com/@robhutton8/entity-framework-vs-repository-pattern-vs-unit-of-work-9fa093bd59e4
-// 2. https://www.thereformedprogrammer.net/is-the-repository-pattern-useful-with-entity-framework-core/
-// 3. https://medium.com/@rabinarayandev/should-you-use-uuids-for-database-keys-597b15b000bb
-// 4. https://blog.stackademic.com/goodbye-swagger-how-net-9-is-redefining-api-documentation-7488456a538f
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-// dodati ovu liniju za swagger
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTeretanaSwaggerGen();
+builder.Services.AddTeretanaCORS();
 
 
-// dodavanje kontaksta baze podataka - dependency injection
-builder.Services.AddDbContext<EdunovaContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext"));
-});
+builder.Services.AddDbContext<EdunovaContext>(opcije => opcije.UseSqlServer(builder.Configuration.GetConnectionString("EdunovaContext")));
 
 
-// Svi se od svuda na sve moguce nacine mogu spojitina na� API
-// �itati https://code-maze.com/aspnetcore-webapi-best-practices/
-//  https://levelup.gitconnected.com/cors-finally-explained-simply-ae42b52a70a3
-builder.Services.AddCors(o => {
-
-    o.AddPolicy("CorsPolicy", builder =>
-    {
-        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    });
-
-});
-
-// automapper
 builder.Services.AddAutoMapper(cfg => {
     cfg.AddProfile<TeretanaMappingProfile>();
 });
 
 
+builder.Services.AddTeretanaSecurity();
+builder.Services.AddAuthorization();
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapOpenApi();
-
+app.UseSwagger();
+app.UseSwaggerUI(opcije => {
+    opcije.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
+    opcije.EnableTryItOutByDefault();
+    opcije.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+});
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
-// dodati ove dvije linije za swagger
-app.UseSwagger();
-app.UseSwaggerUI(o => {
-    o.EnableTryItOutByDefault();
-    o.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
-});
 
 app.MapControllers();
 
-app.UseCors("CorsPolicy");
-
-// za potrebe produkcije
 app.UseStaticFiles();
 app.UseDefaultFiles();
 app.MapFallbackToFile("index.html");
+
+app.UseCors("CorsPolicy");
 
 app.Run();
